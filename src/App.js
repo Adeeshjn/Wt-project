3adceb9c848ff64ad7cc807b58809e88c9b7356f
Adeesh Jain <asij12317@gmail.com>
2021-10-24 23:58:00 +0530

    first commis | Home screen

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './myComponents/Header.js';
+import Body from './myComponents/Body';
+function App() {
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  );
+}
+
+export default App;
---
+++ src/myComponents/Body.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../App.css'
+
+export default function Body() {
+    return (
+
+        <div className="divclass">
+            <aside className="aside">
+                Notifications
+            </aside>
+            <article className="Arti">
+                <div className="span">
+                    Health Blog
+                </div>
+                <div className="span">
+                    Fit Bit tracker
+                </div>
+                <div className="span">
+                    Gyms near you
+                </div>
+            </article>
+        </div>
+
+    )
+}
